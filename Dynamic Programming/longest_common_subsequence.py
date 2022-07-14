4a52ef53311f1f7464c4a84300904f4554778666
douyemiene <viktormiene.vm@gmail.com>
2022-07-14 14:58:20 +0100

    feat: lcs

---
+++ Dynamic Programming/longest_common_subsequence.py
@@ -0,0 +1,52 @@
+import sys
+from unittest import result
+from unittest.util import _Mismatch
+
+
+def lcs(a,b):
+    len_a = len(a)
+    len_b = len(b)
+    n_row = len_b + 1
+    n_col = len_a + 1
+
+    lcs_arr = [[None for i in range(n_col)] for j in range(n_row)]
+    
+    for row in range(n_row):
+        lcs_arr[row][0] = 0
+
+    for col in range(n_col):
+        lcs_arr[0][col] = 0
+
+    for row in range(1, n_row):
+        for col in range(1, n_col):
+            insertion = lcs_arr[ row ][ col-1 ]
+            deletion = lcs_arr[ row-1 ][ col ]
+            mismatch = lcs_arr[ row-1 ][ col-1 ]
+            match = lcs_arr[ row-1 ][ col-1 ] + 1
+
+            if a[col - 1] == b[row - 1]:
+                lcs_arr[row][col] = max(insertion, deletion, match)
+            else:
+                lcs_arr[row][col] = max(insertion, deletion, mismatch)
+
+    return lcs_arr[len_b][len_a]
+
+
+
+if __name__ == '__main__':
+    input = sys.stdin.read()
+    data = list(map(int, input.split()))
+
+    n = data[0]
+    data = data[1:]
+    a = data[:n]
+
+    data = data[n:]
+    m = data[0]
+    data = data[1:]
+    b = data[:m]
+
+    print(lcs(a, b))
+
+
+    
--- Dynamic Programming/primitive_calculator.py
+++ Dynamic Programming/primitive_calculator.py
@@ -1,8 +1,16 @@
+import sys
+
+# recursion isn't always the best way to solve a problem.
+sys.setrecursionlimit(10000)
+
 def minOps(index, n, computed):
+    global ans
+    global calculations
     if index <= 1:
         if index == n:
             return 0
         computed[index] = 0
+        calculations[index] = [index]
         return minOps( index + 1, n, computed)
 
     elif index <= 3:
@@ -10,6 +18,7 @@ def minOps(index, n, computed):
             return 1
 
         computed[index] = 1
+        calculations[index] = [index]
         return minOps( index + 1, n, computed)
     # unsure
     elif index == n + 1:
@@ -20,25 +29,53 @@ def minOps(index, n, computed):
     nOverThree = index // 3
     nModuloThree = index % 3
 
+    c1 = []
+    c2 = []
     if nModuloTwo <= 3:
         forTwo = computed[nOverTwo] + 1 + nModuloTwo
+        if calculations[nOverTwo] == [1]:
+            c1 = [ calculations[nOverTwo][-1] * 2]
+        else:
+            c1 = calculations[nOverTwo] + [ calculations[nOverTwo][-1] * 2]
+        for i in range(1, nModuloTwo + 1):
+            c1.append( c1[-1] + 1  )
     else:
         forTwo = computed[nOverTwo] + 1 + computed[nModuloTwo]
+        c1 = calculations[nOverTwo] + calculations[nModuloTwo]
 
     if nModuloThree <= 3:
         forThree = computed[nOverThree] + 1 + nModuloThree
+        if calculations[nOverThree] == [1]:
+            c2 = [ calculations[nOverThree][-1] * 3]
+        else:
+            c2 = calculations[nOverThree] + [ calculations[nOverThree][-1] * 3]
+        for i in range(1, nModuloThree + 1):
+            c2.append( c2[-1] + 1  )
     else:
         forThree = computed[nOverThree] + 1 + computed[nModuloThree]
+        c2 = calculations[nOverThree] + calculations[nModuloThree]
+
+
+    if forTwo <= forThree:
+        calculations[index] = c1
+    else:
+        calculations[index] = c2
+        
 
     computed[index] = min(forTwo, forThree)
-
+    
     return minOps( index + 1, n, computed )
 
-    
 
 
-
-
-n = 10
+input = sys.stdin.read()
+n = int(input)
+calculations = [ 0 ] * ( n + 1 )
 computed = [0] * ( n + 1)
+ans = [1]
+
 print(minOps( 0, n, computed ))
+calculations[n].insert(0, 1)
+for i in calculations[n]:
+    print(i, end=" ")
+print('')
