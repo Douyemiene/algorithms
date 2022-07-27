5e487606c640f0f969cdcc5fa8f71d06131fc51c
douyemiene <viktormiene.vm@gmail.com>
2022-07-27 12:36:04 +0100

    fix bug in backtrack

--- Dynamic Programming/partitioning_souvenirs.py
+++ Dynamic Programming/partitioning_souvenirs.py
@@ -1,31 +1,18 @@
 import sys
 
 
-values = []
 def backtrack(values, weight, i, n, ansx):
-    #remove ans from weight when done with backtrack call
     if i == 0 and n == 0:
         return ansx
     
-    weight_of_item = weight[ n - 1]
+    weight_of_item = weight[ n - 1 ]
 
-    if (n - 1) >= 0 and values[n][i] - ( values[n - 1][ i - weight_of_item ] + weight_of_item ) == 0:
-            print(f'lost -{weight_of_item}-   v[i][n] {values[n][i]}')
-            # weight.remove(weight_of_item)
+    if (n - 1) >= 0 and i >= weight_of_item and ( values[n - 1][ i - weight_of_item ] + weight_of_item ) > values[n - 1][i]:
             ansx.append(weight_of_item)
             return backtrack(values, weight, i - weight_of_item, n - 1, ansx)
     elif (n - 1) >= 0:
-            print(f'{i},{n - 1}')
             return backtrack(values, weight, i, n - 1, ansx)
     
-
-    print(f'v[n][i] {values[n][i]} values[n - 1][ i - weight_of_item ] {values[n - 1][ i - weight_of_item ]} + {weight_of_item}')
-    print(f'-----------------------{i,n}')
-
-    for i in range(0, 60):
-            for weight in range(0, 60):
-                print(f'{values[i][weight]} ', end='')
-            print('')
 
 def knapsack(W, w):
     n_int = len(w)
@@ -36,7 +23,6 @@ def knapsack(W, w):
 
     for _w in range(W + 1):
         values[0][_w] = 0
-
 
     for i in range(1, n_int + 1):
         for weight in range(1, W + 1):
@@ -50,33 +36,29 @@ def knapsack(W, w):
             if i_was_used > values[i][weight]:
                 values[i][weight] = i_was_used
     
-    # for i in range(0, n_int + 1):
-    #     for weight in range(0, W + 1):
-    #         print(f'{values[i][weight]} ', end='')
-    #     print('')
     ans = []
     collected = backtrack(values, w, W, n_int, ans)
-    return [values[n_int][W], w, collected]
 
-# arr = [1, 2, 3, 4, 5, 5, 7, 7, 8, 10, 12, 19, 25]
-# sum = sum(arr)
+    if collected:
+        for item in collected:
+            if item in w:
+                w.remove(item)
+    else:
+        collected = []
+
+    return [values[n_int][W], w, collected]
 
 
 def partition_souvenirs(sum_x, arr):
     if sum_x % 3 == 0:
         sum_over_3 = sum_x // 3
         [one, arr_two, c1] = knapsack(sum_over_3, arr)
-        # a_two = sum(c1)
-        print()
-        print(f'c1 {c1} arr2 {arr_two}')
+        sum_one = sum(c1)
         [two, arr_three, c2] = knapsack(sum_over_3, arr_two)
-        # a_three = sum(c2)
-        print(f'c2 {c2} arr3 {arr_three}')
+        sum_two = sum(c2)
         [three, arr_four, c3] = knapsack(sum_over_3, arr_three)
-        # a_four = sum(c3)
-        #print(f'{a_two} {a_three} {a_four}')
-        print(f'c3 {c3} arr4 {arr_four}')
-        if one == two and two == three and one + two + three == sum_x:
+        sum_three = sum(c3)
+        if sum_one == sum_two and sum_two == sum_three and sum_one + sum_two + sum_three == sum_x:
             print(1)
         else:
             print(0)
