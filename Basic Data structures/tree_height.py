1798b70e62c6f04021dc97c723d634b254c963e4
douyemiene <viktormiene.vm@gmail.com>
2022-08-04 22:39:10 +0100

    feat: assign children

--- Basic Data structures/tree_height.py
+++ Basic Data structures/tree_height.py
@@ -1,15 +1,25 @@
 class Node:
-    def __init__(self, data, left, right):
+    def __init__(self, data):
         self.data = data
-        self.left = left
-        self.right = right
+        self.children = []
       
 
 
-index_of_nodes = ['4', '-1', '4', '1', '1']
+index_of_nodes = [4, -1, 4, 1, 1]
 nodes = [None] * len(index_of_nodes)
 
-for index, data in enumerate(index_of_nodes):
-    nodes[index] = Node(data, None, None)
+
+for index in range(len(nodes)):
+    nodes[index] = Node(index)
+
+for index, node in enumerate(nodes):
+    index_of_parent =  index_of_nodes[index]
+    parent = nodes[ index_of_parent ]
+
+    parent.children.append(node)
+
+print(nodes[4].data)
 
 
+for n in nodes[4].children:
+    print('data',n.data)
