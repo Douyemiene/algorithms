3acba6c6edba2da57acec3b3ebea804bb4a65e53
douyemiene <viktormiene.vm@gmail.com>
2022-08-07 23:28:24 +0100

    feat: implement buffer process with deque

--- Basic Data structures/network_simulation.py
+++ Basic Data structures/network_simulation.py
@@ -5,15 +5,40 @@ from collections import deque
 Request = namedtuple("Request", ["arrived_at", "time_to_process"])
 Response = namedtuple("Response", ["was_dropped", "started_at"])
 
-
+# should you use a Queue???????????????????????????????????????????????????????
 class Buffer:
     def __init__(self, size):
         self.size = size
-        self.finish_time = []
+        self.finish_time = deque([])
 
     def process(self, request):
-        # write your code here
-        return Response(False, -1)
+        arrival_time = request.arrived_at
+
+        # remove all packets that would have been processed before this one arrives
+        for time in range(len(self.finish_time)):         
+            if arrival_time > time and len(self.finish_time) != 0:
+                self.finish_time.popLeft()           #0(1) * 0(n_requests)
+
+        is_buffer_full = self.size == len(self.finish_time)
+
+        if is_buffer_full:
+            return Response(True, -1)
+
+        last_finish_time = self.finish_time[-1] if len(self.finish_time) != 0 else 0
+      
+        # The time we started processing: arrival time
+        packet_finish_time = arrival_time + request.time_to_process
+        self.finish_time.append(packet_finish_time)  #0(1)
+        
+        if len(self.finish_time) == 0:
+            return Response(False, arrival_time)
+
+       
+        
+        if arrival_time > last_finish_time:
+            return Response(False, arrival_time)
+        else:
+            return Response(False, last_finish_time)
 
 
 def process_requests(requests, buffer):
