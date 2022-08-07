3e17558a2fff7031af1295153992e9a3d7008815
douyemiene <viktormiene.vm@gmail.com>
2022-08-07 23:59:53 +0100

    feat: binary search

---
+++ Divide and conquer/binary_search.py
@@ -0,0 +1,25 @@
+def binary_search(keys, query):
+    low = 0
+    high = len(keys) - 1
+    while low <= high:
+        middle = low + (high - low) // 2
+        if keys[middle] == query:
+            return middle
+        if keys[middle] < query:
+            low = middle + 1
+        else:
+            high = middle - 1
+    return -1
+
+
+if __name__ == '__main__':
+    num_keys = int(input())
+    input_keys = list(map(int, input().split()))
+    assert len(input_keys) == num_keys
+
+    num_queries = int(input())
+    input_queries = list(map(int, input().split()))
+    assert len(input_queries) == num_queries
+
+    for q in input_queries:
+        print(binary_search(input_keys, q), end=' ')
