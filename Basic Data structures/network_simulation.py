d3d66c70d97bb20ea774278e4f898754f66854fb
douyemiene <viktormiene.vm@gmail.com>
2022-08-10 20:59:55 +0100

    feat: network_simulation

--- Basic Data structures/network_simulation.py
+++ Basic Data structures/network_simulation.py
@@ -5,7 +5,6 @@ from collections import deque
 Request = namedtuple("Request", ["arrived_at", "time_to_process"])
 Response = namedtuple("Response", ["was_dropped", "started_at"])
 
-# should you use a Queue???????????????????????????????????????????????????????
 class Buffer:
     def __init__(self, size):
         self.size = size
@@ -15,30 +14,32 @@ class Buffer:
         arrival_time = request.arrived_at
 
         # remove all packets that would have been processed before this one arrives
-        for time in range(len(self.finish_time)):         
-            if arrival_time > time and len(self.finish_time) != 0:
+        for index in range(len(self.finish_time)):         
+            if len(self.finish_time) == 0:
+                break
+            if arrival_time >= self.finish_time[index]:
                 self.finish_time.popleft()           #0(1) * 0(n_requests)
 
         is_buffer_full = self.size == len(self.finish_time)
 
         if is_buffer_full:
             return Response(True, -1)
-
-        last_finish_time = self.finish_time[-1] if len(self.finish_time) != 0 else 0
-      
-        # The time we started processing: arrival time
-        packet_finish_time = arrival_time + request.time_to_process
-        self.finish_time.append(packet_finish_time)  #0(1)
         
         if len(self.finish_time) == 0:
-            return Response(False, arrival_time)
-
-       
-        
-        if arrival_time > last_finish_time:
+            packet_finish_time = arrival_time + request.time_to_process
+            self.finish_time.append(packet_finish_time)  #0(1)
             return Response(False, arrival_time)
         else:
+            # if arrival_time >= last_finish_time:
+            #     return Response(False, arrival_time)
+            # else:
+            last_finish_time = self.finish_time[-1]
+            packet_finish_time = last_finish_time + request.time_to_process
+            self.finish_time.append(packet_finish_time)  #0(1)
             return Response(False, last_finish_time)
+
+        
+       
 
 
 def process_requests(requests, buffer):
@@ -54,9 +55,6 @@ def main():
     for _ in range(n_requests):
         arrived_at, time_to_process = map(int, input().split())
         requests.append(Request(arrived_at, time_to_process))
-
-    if n_requests == 0:
-        print(0)
 
     buffer = Buffer(buffer_size)
     responses = process_requests(requests, buffer)
