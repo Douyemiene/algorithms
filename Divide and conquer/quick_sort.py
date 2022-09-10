83cec5934ce2089c138be1fbd93a017c518a3858
douyemiene <viktormiene.vm@gmail.com>
2022-09-10 23:22:34 +0100

    feat: quick_sort

---
+++ Divide and conquer/quick_sort.py
@@ -0,0 +1,56 @@
+# Uses python3
+import sys
+import random
+
+def partition3(a, l, r):
+    #write your code here
+    x = a[r]
+    j = l
+    #print(f'j1 {j}')
+    for i in range(l + 1, r + 1):
+        if a[i] != x:
+            if i - j > 1:
+                a[i], a[j+1] = a[j+1], a[i]
+            j += 1
+    #print(f'j2 {j}')
+    a[l], a[j] = a[j], a[l]
+    return j
+
+def partition2(a, l, r):
+    x = a[l]
+    j = l
+    for i in range(l + 1, r + 1):
+        if a[i] <= x:
+            j += 1
+            a[i], a[j] = a[j], a[i]
+    a[l], a[j] = a[j], a[l]
+    return j
+
+
+def randomized_quick_sort(a, l, r):
+    if l >= r:
+        return
+    k = random.randint(l, r)
+    a[l], a[k] = a[k], a[l]
+    #use partition3
+    m = partition2(a, l, r)
+    #element to partition by is a[m]
+    n = partition3(a, l, m)
+    randomized_quick_sort(a, l, n );
+    randomized_quick_sort(a, m + 1, r);
+
+# if __name__ == '__main__':
+#     input = sys.stdin.read()
+#     #n, *a = list(map(int, input.split()))
+#     a = [10,9,1,2,9,10,9,3,4,6,9,10]
+#     partition3(a, 0, 12 - 1)
+#     for x in a:
+#         print(x, end=' ')
+
+
+if __name__ == '__main__':
+    input = sys.stdin.read()
+    n, *a = list(map(int, input.split()))
+    randomized_quick_sort(a, 0, n - 1)
+    for x in a:
+        print(x, end=' ')
