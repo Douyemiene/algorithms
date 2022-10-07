db1822a845958982db60bfde509649187f82143e
douyemiene <viktormiene.vm@gmail.com>
2022-10-07 19:55:40 +0100

    fix: remove python3 syntax from two sum

--- others/two_sum.py
+++ others/two_sum.py
@@ -1,5 +1,5 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums, target):
         complements = {}
         for index, value in enumerate(nums):
             if value in complements:
