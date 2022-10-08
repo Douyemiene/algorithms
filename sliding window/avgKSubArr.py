e793fc885969067e90602359bc4f46b4377b1dda
douyemiene <viktormiene.vm@gmail.com>
2022-10-08 20:00:56 +0100

    feat: average of k sub arr

---
+++ sliding window/avgKSubArr.py
@@ -0,0 +1,30 @@
+import re
+from unittest import result
+
+
+def maxsubarr(arr,k):
+    arrLen = len(arr)
+
+    if k == 0 or k > arrLen:
+        return []
+    
+    result = []
+    stopIndex = arrLen - k
+    prevSum = 0
+
+    for i in range(k):
+        prevSum += arr[i]
+
+    result.append(prevSum / k)
+
+    for i in range(1, stopIndex + 1):
+        sum = prevSum - arr[i -1] + arr[i + k - 1]    
+        result.append(sum/k)
+        prevSum = sum
+
+    return result
+
+
+print(maxsubarr([1,3,2,6,-1,4,1,8,2], 5))
+
+
