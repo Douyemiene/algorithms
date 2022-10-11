10cc14e1a4fd077298db8ac9ba489e94b5c9a2e0
douyemiene <viktormiene.vm@gmail.com>
2022-10-11 23:01:07 +0100

    name variables properly

--- sliding window/max_sum_subarr.py
+++ sliding window/max_sum_subarr.py
@@ -1,25 +1,28 @@
-def max_sum_subarr(arr, k):
-    arrLen = len(arr)
+def max_sum_subarr(nums, k):
+arrLen = len(nums)
 
-    if k == 0 or k > arrLen:
-        None
+if k == 0 or k > arrLen:
+    None
 
-    stopIndex = arrLen - k
-    firstSum = 0
+stopIndex = arrLen - k
+firstSum = 0
 
-    for i in range(0, k):
-        firstSum += arr[i]
+for i in range(0, k):
+    firstSum += nums[i]
 
-    maxSum = firstSum
-    startOfMaxSubArr = 0
+maxSum = firstSum
+prevSum = firstSum
+startOfMaxSubArr = 0
 
-    for i in range(1, stopIndex + 1):
-        newSum = maxSum - arr[i-1] + arr[i]
+for i in range(1, stopIndex + 1):
+    newSum = prevSum - nums[i-1] + nums[i+k-1]
 
-        if newSum > maxSum:
-            startOfMaxSubArr = i
-            maxSum = newSum
-               
-    return arr[startOfMaxSubArr : startOfMaxSubArr + k]
+    if newSum > maxSum:
+        startOfMaxSubArr = i
+        maxSum = newSum
+
+    prevSum = newSum
+            
+return nums[startOfMaxSubArr : startOfMaxSubArr + k]
 
 print(max_sum_subarr([-1,-2,3,4], k = 3))
