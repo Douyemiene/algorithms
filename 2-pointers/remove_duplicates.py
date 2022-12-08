4086ba123ac4facd35f3844a00759311723bdfc7
Douyemiene <viktormiene.vm@gmail.com>
2022-12-08 17:46:50 +0100

    remove_duplicates

---
+++ 2-pointers/remove_duplicates.py
@@ -0,0 +1,17 @@
+# Problem Statement
+# Given a sorted array, create a new array containing squares of all the numbers of the input array in the sorted order.
+
+# Example 1:
+
+# Input: [-2, -1, 0, 2, 3]
+# Output: [0, 1, 4, 4, 9]
+
+# compare both halves and place one first
+
+
+# 4 1 0 2 3
+
+
+# def sqr():
+    
+
