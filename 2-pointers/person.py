021801307f1788c526dfddd608104226bcd118d8
Douyemiene <viktormiene.vm@gmail.com>
2022-12-29 19:32:47 +0100

    python class example

---
+++ 2-pointers/person.py
@@ -0,0 +1,44 @@
+class Human:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def sayName(self):
+        print(f'Hi, my name is {self.name}')
+
+
+
+def main():
+    vic = Human('douye','12')
+    vic.sayName()
+
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
