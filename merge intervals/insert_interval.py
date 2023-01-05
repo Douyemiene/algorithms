dad299ba48558912144520842c04648fd4aef598
Douyemiene <viktormiene.vm@gmail.com>
2023-01-06 00:32:43 +0100

    fix: insert interval

--- merge intervals/insert_interval.py
+++ merge intervals/insert_interval.py
@@ -1,32 +1,37 @@
-def mergeIntervals(intervals, target):
-    intervals.sort(key = lambda i: i[0])
+class Solution(object):
+    def insert(self, intervals, target):
+        intervals.sort(key=lambda i: i[0])
 
-    merged_intervals = []
-    start_index, end_index = 0, 1
-   
-    for start, end in intervals:
-        if end < target[start_index] or start > target[end_index]:
-            merged_intervals.append([start, end])
-        else:
-            min_start = min(start, target[start_index])
-            max_end = max(end, target[end_index])
+        merged_intervals = []
+        start_index, end_index = 0, 1
+        last_index = len(intervals) - 1
+        count = -1
 
-            merged_intervals.append([min_start, max_end])
+        if len(intervals) == 0:
+            return [target]
 
+        for index, interval in enumerate(intervals):
+            start, end = interval
+            interval_overlaps_target = start <= target[end_index]
 
+            if end < target[start_index]:
+                merged_intervals.append([start, end])
 
-    return merged_intervals
+                if index == last_index:
+                    merged_intervals.append(target)
+            elif interval_overlaps_target:
+                min_start = min(start, target[start_index])
+                max_end = max(end, target[end_index])
+                target = [min_start, max_end]
 
+                if index == last_index:
+                    merged_intervals.append(target)
+            else:
+                count += 1
+                if count == 0:
+                    merged_intervals.append(target)
+                merged_intervals.append([start, end])
 
-def main():
-  print("Intervals after inserting the new interval: " +
-        str(mergeIntervals([[1, 3], [5, 7], [8, 12]], [4, 6])))
-  print("Intervals after inserting the new interval: " +
-        str(mergeIntervals([[1, 3], [5, 7], [8, 12]], [4, 10])))
-  print("Intervals after inserting the new interval: " +
-        str(mergeIntervals([[2, 3], [5, 7]], [1, 4])))
+        return merged_intervals
 
-
-main()
-
-
+        return merged_intervals
